04ed233a5b782c972d1bc1cda651eaf851148a86
Thunder <97145312+BytexThunder@users.noreply.github.com>
2022-02-21 09:11:06 +0700

    Update xyz.py

--- xyz.py
+++ xyz.py
@@ -2,7 +2,9 @@
 
 import os
 import threading
-from flask import Flask
+from flask import Flask, render_template
+
+threads = []
 
 os.environ['Flas_ENV'] = 'development'
 app = Flask(__name__)
@@ -10,7 +12,7 @@ app.debug = False
 
 @app.route('/')
 def home():
-    return 'Code: 999666'
+    return render_template('index.html')
 
 print("\n * Go to http://127.0.0.1:5000/")
 threading.Thread(target=app.run, kwargs={'use_reloader': False}).start()
