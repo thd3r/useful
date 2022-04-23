1184c99650cf15ebab5a7e4a9b027b732a61be8d
Thunder <97145312+0xthd3r@users.noreply.github.com>
2022-04-23 22:45:10 +0700

    Add files via upload

---
+++ ransom/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='Ransom_software',
+    version='1.0',
+    python_requires='>=2.7',
+    packages=find_packages()+['.'],
+    include_package_data=True,
+    url='https://github.com/0xthd3r/Useless-but-useful',
+    license='GPL-3.0',
+    author='Thunder',
+    description='Ransomware virus for windows machine'
+)
